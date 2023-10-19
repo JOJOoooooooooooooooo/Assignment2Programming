d111d781ce5e1c67f60bee0d242628210f5083da
Jojo <jghattas7@outlook.com>
2023-10-19 19:05:31 -0400

    adjusting user selection

--- App.py
+++ App.py
@@ -1,19 +1,36 @@
 import PizzaClass
 
-SmallPizza = PizzaClass.SmallPizza
 
-MediumPizza = PizzaClass.MediumPizza
-
-LargePizza = PizzaClass.LargePizza
-
-XLargePizza = PizzaClass.XLargePizza
 
 print("What size Pizza would you like?")
 input("1: Small, 2: Medium, 3: Large or 4: Extra Large")
 
 
-userselection = input("Please select one: ")
+userselection = input("Please select an option: ")
 
-if userselection ==
-    print(userselection + " Good Choice!")
-    print("One pizza of this size")
+x = 0 
+
+while x == 0: 
+
+    if userselection == "1":
+        x += 1
+        print("Small Pizza, Good choice!")
+        Pizza = PizzaClass.SmallPizza
+
+    elif userselection == '2':
+        x += 1
+        print("Medium Pizza, Good choice!")
+        Pizza = PizzaClass.MediumPizza
+    
+    elif userselection == '3':
+        x += 1
+        print("Large Pizza, Good choice!")
+        Pizza = PizzaClass.LargePizza
+
+    elif userselection == '4':
+        x =+ 1
+        print("Extra Large Pizza, Good choice!")
+        Pizza = PizzaClass.XLargePizza
+
+    else:
+        print("Invalid input")
