63ad5513a56e45c7695ad554cc01966422b807d6
Jojo <jghattas7@outlook.com>
2023-10-19 18:56:39 -0400

    Creating app progression

--- App.py
+++ App.py
@@ -1,6 +1,19 @@
+import PizzaClass
+
+SmallPizza = PizzaClass.SmallPizza
+
+MediumPizza = PizzaClass.MediumPizza
+
+LargePizza = PizzaClass.LargePizza
+
+XLargePizza = PizzaClass.XLargePizza
 
 print("What size Pizza would you like?")
-input("Small, Medium, or Large")
-print("Please select one")
+input("1: Small, 2: Medium, 3: Large or 4: Extra Large")
 
-userselection = input("small, medium or large")
+
+userselection = input("Please select one: ")
+
+if userselection ==
+    print(userselection + " Good Choice!")
+    print("One pizza of this size")
--- PizzaClass.py
+++ PizzaClass.py
@@ -1,8 +1,16 @@
-class Pizza():
+class SmallPizza():
     def __init__(self):
         self.SmallPizza = 10
-        self.MediumPizza = 12
-        self.LargePizza = 15
-        self.XLargePizza = 18
 
+class MediumPizza():
+    def __init__(self):
+        self.Medium = 12
+
+class LargePizza():
+    def __init__(self):
+        self.Large = 15
+
+class XLargePizza():
+    def __init__(self):
+        self.XLarge = 18
 
